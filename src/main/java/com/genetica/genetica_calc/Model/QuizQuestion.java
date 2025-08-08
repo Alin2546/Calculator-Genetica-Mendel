ba78565747e1c661c3c3a940d6e3d9cbc681223f
Alin <eternalsq1337@gmail.com>
2025-08-08 22:13:19 +0300

    refactored style ing

---
+++ src/main/java/com/genetica/genetica_calc/Model/QuizQuestion.java
@@ -0,0 +1,11 @@
+package com.genetica.genetica_calc.Model;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class QuizQuestion {
+    private String text;
+    private String optionA;
+    private String optionB;
+    private String optionC;
+}
